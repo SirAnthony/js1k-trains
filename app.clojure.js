8ea79102881a67c15e918fe283e75a8dcf6c60a2
SirAnthony <anthony@adsorbtion.org>
2015-02-22 23:59:33 +0500

    1268 broken

--- app.clojure.js
+++ app.clojure.js
@@ -20,28 +20,28 @@ T=function(x,y,z){
         e=z&&T(D,S,z-1), U=S.X, V=S.Y,
         X=U, Y=V,
     N=function(x,y,z){
-        z = Math.abs(K[n]),
-        f = O[10*(x+(g&1)*K[n]/g)+(y+(g&2)*K[n]/g/2)];
+        z = Math.abs(K[n])
+        f = O[10*(x+(z&1)*K[n]/z)+(y+(z&2)*K[n]/z/2)]
         return f && (f.x!=x && f.t&j.t&1 ||
-             (f.y!=y && f.t&j.t&2)) && (j=f);
+             (f.y!=y && f.t&j.t&2)) && (j=f)
     },
     P=function(x,y,z){
-        n=(n+3)%4;
-        for(i=0;i<4&&!N(x,y);i++)
-            n=(n+1)%4;
+        n=(n+3)%4
+        for(i=0;i<4&&!N(x,y,n);i++)
+            n=(n+1)%4
     }
     s.T=function(x,y,z){
-        c.fillStyle=l(F);
-        c.fillRect(U,V,6,6);
-        t=Math.atan2(Y-V,X-U);
-        U+=.05*(x|0)*Math.cos(t);
-        V+=.05*(x|0)*Math.sin(t);
-        4>((X-U)*(X-U)+(Y-V)*(Y-V)) &&
+        z = Math.abs(K[n])
+        c.fillStyle=l(F)
+        c.fillRect(U,V,6,6)
+        U+=.05*x*(z&1)*K[n]/z
+        V+=.05*x*(z&2)*K[n]/z/2
+        4>(Math.abs(X-U)+Math.abs(Y-V)) &&
             (P(j.x,j.y), X=W*(j.x+.5), Y=H*(j.y+.5));
-        0<=R.indexOf(s) && 30>((D.X-U)*(D.X-U)+(D.Y-V)*(D.Y-V)) &&
+        s in R && 30>(Math.abs(D.X-U)+Math.abs(D.Y-V)) &&
             (A+=f,B++, R.splice(R.indexOf(s), 1));
         f-=x/1e3
-        e&&e.T(C.shift())
+        e&&e.T(C.shift()|0)
         C.push(x)
     }
     return s
@@ -92,7 +92,7 @@ c.font = "35px Sans";
     O.forEach(U)
     R.forEach(U)
     c.fillStyle = "#FFF";
-    1<Math.abs(A)/2e4&&c.fillText("Trains: "+B, a.width/2.5, a.height/2);
+    1<Math.abs(A)/1e4&&c.fillText("Trains: "+B, a.width/2.5, a.height/2);
     c.fillText(B, a.width, 35);
     c.fillText(~~A, 0, 35);
 })();
