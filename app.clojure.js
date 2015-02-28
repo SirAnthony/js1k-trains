3e1c89aaf67a5d57159e9255b5c6da169da12228
Sir Anthony <anthony@adsorbtion.org>
2015-02-28 17:30:17 +0500

    fix all, 1130

--- app.clojure.js
+++ app.clojure.js
@@ -3,28 +3,27 @@ T=function(x,y,z){
     var s={F: y.F},
         C=Array(9), j=x, J=y,
         U=W*(j.x+.5), V=H*(j.y+.5),
-        n=j.t, e=0, //e=z&&T(x,y,z-1),
+        n=j.t, e=z&&T(x,y,z-1),
         X=U, Y=V,
-        n=1, g=-1;
+        n=1, g=-1,
     P=function(x,y,z){
+        n&2&&(g*=-1), n=n*2%3;
         for(i=0;i<5;i++){
-            n&2&&(g*=-1);
-            n=(n+1)%3;
             f = O[10*(x+(n&1)*g)+(y+(n&2)*g/2)];
-            f && f.t&j.t&n && (j=f, X=W*(j.x+.5), Y=H*(j.y+.5), i=4)
+            f && f.t&j.t&n && (j=f, X=W*(j.x+.5), Y=H*(j.y+.5), i=4) ||
+             (n=n*2%3, n&2&&(g*=-1));
         }
     }
     s.T=function(x,y,z){
         c.fillStyle="hsl("+360/I*s.F+",70%,50%)"
         c.fillRect(U,V,6,6)
-        3>(Math.abs(X-U)+Math.abs(Y-V)) && (
+        x && (2>(Math.abs(X-U)+Math.abs(Y-V)) && (
             // Remove
-            s in R && j==J && (A+=1e2,B++, R.splice(R.indexOf(s), 1)) ||
+            R.indexOf(s)>=0 && j==J && (A+=1e2,B++, R.splice(R.indexOf(s), 1)) ||
             // Next point
-            P(j.x,j.y), 1)
-        || (U+=.05*x*(n&1)*g, V+=.05*x*(n&2)*g/2)
-        e&&e.T(C.shift()|0)
-        C.push(x)
+            P(j.x,j.y))
+        || (U+=x*(n&1)*g, V+=x*(n&2)*g/2));
+        e&&(e.T(C.shift()||0), C.push(x))
     }
     return s
 }
@@ -49,16 +48,16 @@ a.onclick=function(x,y,z){
     O[10*(x.pageX/W|0)+x.pageY/H|0].t = (O[10*(x.pageX/W|0)+x.pageY/H|0].t+1)%4;
 };
 V=0, F=Date.now(), E=0, A=0, B=0, P=1, I=0;
-U=function(x,y,z){ x.T(V) };
+U=function(x,y,z){ x.T(16/20) };
 c.font = "35px Sans";
 (G=function(x,y,z){
     requestAnimationFrame(G);
     V = Date.now() - F
     E += V
     F += V
-    if (1>Math.abs(A)/1e4 && !(/*2*I*/0<R.length || .8>E%Math.random()))
-        d = C[1e2*Math.random()%I|0], b = C[1e2*Math.random()%I|0],
-        d!=b && d.t && R.push(T(d,b,1e2*Math.random()%I|0+1));
+    if (1>Math.abs(A)/1e4 && !(2*I<R.length || .8>E%Math.random()))
+        x = C[1e2*Math.random()%I|0], y = C[1e2*Math.random()%I|0],
+        x!=y && x.t && R.push(T(x,y,1e2*Math.random()%I|0+1));
     // -> 1 call of C -> move from func
     if (1>Math.abs(A)/1e4 && E/4e4>I-3 && 10>I-3)
         x=O[1e2*Math.random()|0], I = x.F = C.push(x);
